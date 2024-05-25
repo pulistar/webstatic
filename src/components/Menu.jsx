283c6a871b72a34433422dcab296cf47947d70e9
pulistar <141847841+pulistar@users.noreply.github.com>
2024-05-25 18:33:20 -0500

    Update Menu.jsx
    pi

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,25 +1,20 @@
-// Menu.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 const Menu = () => {
-  return (
-    <>
-      <header className='flex items-center justify-between p-10'>
-        <Link to="/" className='text-white font-roboto text-3xl tracking-wider flex items-center'> Salud y Bienestar</Link>
-        <nav className='space-x-4'>
-          <div className='ssm:hidden log:block space-x-2'>
-            <Link to="/actividades" className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl'>Gestión de Actividades</Link>
-            <Link to="/alimentacion" className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl'>Gestión de Alimentación</Link>
-            <Link to="/recordatorios" className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl'>Gestión de Recordatorios</Link>
-          </div>
-        </nav>
-        
-      </header>
-    </>
-  );
+    return (
+        <header className="flex justify-end mr-4">
+            <div>
+                <Link to='/'>
+                    <h1 className="my-4 text-6xl font-extrabold  md:text-left">
+                        <span className="hidden md:inline">Salud</span>
+                        <span className="text-blue-600">Bienestar</span>
+                    </h1>
+                </Link>
+            </div>
+        </header>
+    );
 }
 
 export default Menu;
